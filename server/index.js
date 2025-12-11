34e57cbdd54681e94637aec27b9d04eba1eb2de8
michaelbevan19 <michaelbevan2020@gmail.com>
2025-12-11 13:44:15 +0530

    Fix CORS error

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ["https://splendorous-lolly-7e568b.netlify.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 // Database Connection
